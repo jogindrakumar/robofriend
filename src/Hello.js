f71862feddf7792aaac5d0045b2b33ce2597abee
jogindrakumar <joginder9868@outlook.com>
2020-01-09 11:33:34 +0530

    make a component hello.js

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import 'tachyons';
+
+class Hello extends Component{
+    render(){
+        return (
+
+<div className='f1 tc dark-green'>
+<h1>hello world</h1>
+</div>
+
+        );
+    }
+}
+export default Hello;
