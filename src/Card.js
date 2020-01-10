0d7dab79a1fa78a6f28efcd7465ab3bf823ff084
jogindrakumar <joginder9868@outlook.com>
2020-01-10 11:36:58 +0530

    addCard.js

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+const Card = ()=>{
+    return(
+        <div>
+
+<div>
+            <img src='https://robohash.org/test?200*200'/>
+        </div>
+        <div>
+            <h2>john doe</h2>
+            <p>johndoe@gmail.com</p>
+        </div>
+
+
+        </div>
+
+
+
+    );
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<div>
+
+                <Card/>
+
+</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
