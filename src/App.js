1022aa7e50579d07833ffebdc909478bb884d7db
jogindrakumar <joginder9868@outlook.com>
2020-01-11 12:00:40 +0530

    AddsearchBoxAndapp.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+
+
+const App = () =>{
+
+    return (
+            <div className='tc'>
+                <h1>Robo Friend</h1>
+                <SearchBox/>
+                <CardList robots={robots}/>
+            </div>
+    );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
 
+import App from './App';
 
-import Card from './Card';
-import {robots} from './robots';
+
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div>
-
-                <Card id = {robots[0].id}name={robots[0].name}email={robots[0].email}/>
-                <Card id = {robots[1].id}name={robots[1].name}email={robots[1].email}/>
-                <Card id = {robots[2].id}name={robots[2].name}email={robots[2].email}/>
-                <Card id = {robots[3].id}name={robots[3].name}email={robots[3].email}/>
-                <Card id = {robots[4].id}name={robots[4].name}email={robots[4].email}/>
-                <Card id = {robots[5].id}name={robots[5].name}email={robots[5].email}/>
-                <Card id = {robots[6].id}name={robots[6].name}email={robots[6].email}/>
-                <Card id = {robots[7].id}name={robots[7].name}email={robots[7].email}/>
-                <Card id = {robots[8].id}name={robots[8].name}email={robots[8].email}/>
-                <Card id = {robots[9].id}name={robots[9].name}email={robots[9].email}/>
-                <Card id = {robots[10].id}name={robots[10].name}email={robots[10].email}/>
-                <Card id = {robots[11].id}name={robots[11].name}email={robots[11].email}/>
-
-</div>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
