b61004bde40981bd3209160ca643ae274ac1e89f
jogindrakumar <joginder9868@outlook.com>
2020-01-13 11:50:32 +0530

    AddOnSearchEventFun

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
 
 
-const App = () =>{
 
-    return (
+
+class App extends Component{
+
+    constructor(){
+        super()
+        this.state ={
+            robots : robots,
+            searchfield : ''
+        }
+    }
+    OnsearchChange = (event) =>{
+       this.setState({searchfield: event.target.value})
+        
+        }
+        
+    
+    render(){
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+            return (
             <div className='tc'>
                 <h1>Robo Friend</h1>
-                <SearchBox/>
-                <CardList robots={robots}/>
+                <SearchBox searchChange={this.OnsearchChange}/>
+                <CardList robots={filteredRobots}/>
             </div>
     );
+    }
 }
+
+
 export default App;
